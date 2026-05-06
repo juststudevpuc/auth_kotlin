bafdf4c4c95475d4949fa614caafac0a8c2007e6
chroengbuntem-create <chroengbuntem@gmail.com>
2026-05-06 15:29:52 +0700

    Add FirebaseAuth initialization in SigninActivity

--- app/src/main/java/com/example/authfyab/ui1/auth/SigninActivity.kt
+++ app/src/main/java/com/example/authfyab/ui1/auth/SigninActivity.kt
@@ -14,6 +14,8 @@ class SigninActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySigninBinding
 
+    private lateinit var auth: FirebaseAuth
+
     // 1. Declare the Manager (ViewModel) instead of Firebase
     private lateinit var authViewModel: AuthViewModel
 
